5d9513db7386417c018501343753a179fe8e39f6
Mirco Kraenz <kraenzmirco@gmail.com>
2022-11-23 11:01:38 +0900

    feat: custom resource for player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,20 +3,12 @@ extends KinematicBody2D
 
 var velocity := Vector2.ZERO
 
-const max_speed := 50.0
-const acceleration := 300.0
-const friction := 400.0
-const gravity := 4
-const after_jump_apex__extra_gravity := 2
-const jump_strength = 140
-const jump_release_force = 40
-const max_fall_speed = 200.0
-const die_over_y = 200
+export (Resource) var move_data # : PlayerMovementData
 
 onready var sprite = $AnimatedSprite
 
 func _physics_process(delta):
-	if position.y > die_over_y:
+	if position.y > move_data.die_over_y:
 		die()
 
 	var input_x = Input.get_action_strength("move_right") - \
@@ -34,9 +26,9 @@ func _physics_process(delta):
 	var jump_input = Input.is_action_just_pressed("jump")
 	var jump_released = Input.is_action_just_released("jump")
 	# the player can somewhat stop the jump but only close to the initial stremf
-	var is_far_from_apex = velocity.y < -jump_release_force 
+	var is_far_from_apex = velocity.y < -move_data.jump_release_force 
 	if is_on_floor() and jump_input:
-		velocity.y = -jump_strength
+		velocity.y = -move_data.jump_strength
 	
 	if not is_on_floor():
 		sprite.play("jump")
@@ -45,7 +37,7 @@ func _physics_process(delta):
 		# variable jump height
 		velocity.y = 0
 	if not is_on_floor() and velocity.y > 0:
-		velocity.y += after_jump_apex__extra_gravity
+		velocity.y += move_data.after_jump_apex__extra_gravity
 		
 	var was_in_air = not is_on_floor()
 
@@ -58,16 +50,16 @@ func _physics_process(delta):
 		sprite.play("run", true)
 
 func apply_gravity():
-	 velocity.y += gravity
-	 velocity.y = min(velocity.y, max_fall_speed)
+	 velocity.y += move_data.gravity
+	 velocity.y = min(velocity.y, move_data.max_fall_speed)
 
 func apply_friction(delta: float):
 	velocity.x = velocity.move_toward(Vector2.ZERO, \
-		friction * delta).x
+		move_data.friction * delta).x
 
 func apply_acceleration(input_x: float, delta: float):
-		velocity.x = move_toward(velocity.x, input_x * max_speed, \
-			acceleration * delta)
+		velocity.x = move_toward(velocity.x, input_x * move_data.max_speed, \
+			move_data.acceleration * delta)
 
 func die():
 	var _a = get_tree().reload_current_scene()
---
+++ Player/PlayerMovementData.gd
@@ -0,0 +1,12 @@
+class_name PlayerMovementData
+extends Resource
+
+export var max_speed := 50.0
+export var acceleration := 300.0
+export var friction := 400.0
+export var gravity := 4.0
+export var after_jump_apex__extra_gravity := 2
+export var jump_strength = 140
+export var jump_release_force = 40
+export var max_fall_speed = 200.0
+export var die_over_y = 200
