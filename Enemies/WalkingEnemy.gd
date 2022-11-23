98a86adce815125fa976eddebb36b85b27af0f1f
Mirco Kraenz <kraenzmirco@gmail.com>
2022-11-23 10:20:47 +0900

    feat: walking enemy stays away from edges

--- Enemies/WalkingEnemy.gd
+++ Enemies/WalkingEnemy.gd
@@ -6,16 +6,24 @@ var velocity = Vector2.ZERO
 var direction = Vector2.RIGHT
 
 onready var sprite = $AnimatedSprite
+onready var ledge_check = $LedgeCheck
 
 func _ready():
 	sprite.play("walk")
+	sprite.flip_h = direction.x > 0
 
 func _physics_process(_delta):
 	var found_wall = is_on_wall()
 	if found_wall:
-		direction *= -1
+		reverse_direction()
 
-	velocity = direction * max_speed
-	velocity = move_and_slide(velocity, Vector2.UP)
+	var _a = move_and_slide(direction * max_speed, Vector2.UP)
 
+	var near_edge = not ledge_check.is_colliding()
+	if near_edge:
+		reverse_direction()
+
+func reverse_direction():
+	direction *= -1
+	ledge_check.position.x *= -1
 	sprite.flip_h = direction.x > 0
