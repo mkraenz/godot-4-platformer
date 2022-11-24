d1a9726eca7dd751ccb6a8a6194627c6f438424c
Mirco Kraenz <kraenzmirco@gmail.com>
2022-11-24 11:46:57 +0900

    refactor: tool for spiky

--- Enemies/MovingSpiky.gd
+++ Enemies/MovingSpiky.gd
@@ -1,3 +1,4 @@
+tool
 extends Path2D
 
 enum AnimationType {
@@ -5,16 +6,31 @@ enum AnimationType {
 	Bounce,
 }
 
-export(AnimationType) var animation_type := AnimationType.Loop
-export(float) var playback_speed = 1.0
-export var reverse_direction = false
+# setters necessary to make the `tool` work
+export(AnimationType) var animation_type = AnimationType.Loop setget set_animation_type
+export(float) var playback_speed = 1.0 setget set_playback_speed
+export var reverse_direction = false setget set_reverse_direction
 
-onready var anims = $AnimationPlayer as AnimationPlayer
+onready var anims : AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
+	animate()
+		
+func set_playback_speed(speed : float) -> void:
+	playback_speed = speed
+	animate()
+	
+func set_reverse_direction(reverse : bool) -> void:
+	reverse_direction = reverse
+	animate()
+	
+func set_animation_type(type: int) -> void:
+	animation_type = type
+	animate()
+
+func animate():
 	match animation_type:
 		AnimationType.Loop:
 			anims.play("move_along_path_loop", -1, playback_speed, reverse_direction)
 		AnimationType.Bounce:
 			anims.play("move_along_path_bounce", -1, playback_speed, reverse_direction)
-		
