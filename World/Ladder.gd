03260caf33eae0d7f62ee8c461a0d822d589933a
Mirco Kraenz <kraenzmirco@gmail.com>
2022-11-26 08:34:50 +0900

    fix: ladder switch works again

--- World/Ladder.gd
+++ World/Ladder.gd
@@ -1,2 +1,5 @@
 class_name Ladder
 extends Area2D
+
+func _ready() -> void:
+    print(get_path())
--- World/Switch.gd
+++ World/Switch.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
-signal switch_on_ladder1
+# absolute path to the object that gets activated by the switch
+export var effect_node_path := "/root/Playground/Interactables/Ladder1"
 
 onready var sprite = $AnimatedSprite
 onready var audio = $AudioStreamPlayer
+
+var effect_node: Node = null
+var parent: Node = null
 
 enum State {
 	On,
@@ -11,9 +15,20 @@ enum State {
 }
 var state = State.Off
 
+func _ready() -> void:
+	deactivate_effect()
+
 func _on_Switch_body_entered(_body: Player) -> void:
 	if state == State.Off:
 		state = State.On
 		sprite.play("on")
 		audio.play()
-		emit_signal("switch_on_ladder1")
+		active_effect()
+
+func active_effect() -> void:
+	parent.call_deferred("add_child", effect_node)
+
+func deactivate_effect() -> void:
+	effect_node = get_node(effect_node_path)
+	parent = effect_node.get_parent()
+	parent.remove_child(effect_node)
