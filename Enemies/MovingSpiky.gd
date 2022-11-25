dfb37860dc7cabd4c2e5c50c221cb44aeda68412
Mirco Kraenz <kraenzmirco@gmail.com>
2022-11-25 09:39:32 +0900

    fix: animplayer needs reverse_direction=true AND negative playback_speed to play backwards

--- Enemies/MovingSpiky.gd
+++ Enemies/MovingSpiky.gd
@@ -8,7 +8,7 @@ enum AnimationType {
 
 # setters necessary to make the `tool` work
 export(AnimationType) var animation_type = AnimationType.Loop setget set_animation_type
-export(float) var playback_speed = 1.0 setget set_playback_speed
+export(float, 0, 100) var playback_speed = 1.0 setget set_playback_speed
 export var reverse_direction = false setget set_reverse_direction
 
 onready var anims : AnimationPlayer = $AnimationPlayer
@@ -29,8 +29,11 @@ func set_animation_type(type: int) -> void:
 	animate()
 
 func animate():
+	var playback_speed_factor = -1 if reverse_direction else 1
+	# to play anim backwards, we need
+	var speed = playback_speed * playback_speed_factor
 	match animation_type:
 		AnimationType.Loop:
-			anims.play("move_along_path_loop", -1, playback_speed, reverse_direction)
+			anims.play("move_along_path_loop", -1, speed, reverse_direction)
 		AnimationType.Bounce:
-			anims.play("move_along_path_bounce", -1, playback_speed, reverse_direction)
+			anims.play("move_along_path_bounce", -1, speed, reverse_direction)
