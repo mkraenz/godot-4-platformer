3f8a9130eca48767517080186bebceb7ac004587
Mirco Kraenz <kraenzmirco@gmail.com>
2022-11-30 22:04:09 +0900

    feat: start game with space bar

--- UI/TitleMenu.gd
+++ UI/TitleMenu.gd
@@ -2,6 +2,11 @@ extends Control
 
 export (PackedScene) var level
 
+func _unhandled_input(_event: InputEvent):
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().set_input_as_handled() # stop event propagation
+		_on_StartButton_pressed()
+
 func _on_QuitButton_pressed() -> void:
 	get_tree().quit()
 
